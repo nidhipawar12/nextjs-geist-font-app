c2999a0d77774e1510ffa42993c7dc3a6104ce83
nidhipawar12 <2023000069@mssu.ac.in>
2025-07-20 14:49:54 +0530

    Add .next/static/chunks/src_app_page_tsx_3d22f4d5._.js 1753003194027

---
+++ .next/static/chunks/src_app_page_tsx_3d22f4d5._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_a31de8ea._.js",
+  "static/chunks/src_9a316a07._.js"
+],
+    source: "dynamic"
+});
